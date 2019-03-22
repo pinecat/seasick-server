114c27e19c12ad42ecf9a2cbc2b112cc988e1b1b
rory <rory.dudley@gmail.com>
2019-03-21 23:21:13 -0400

    created Server class for listening for requests and started to create some basic subroutines

---
+++ src/main/java/net/arch64/gofish/seasick/serve/Server.java
@@ -0,0 +1,38 @@
+/* Package */
+package net.arch64.gofish.seasick.serve;
+
+/* Imports */
+import java.net.*;
+import java.io.*;
+
+/* Class: Server */
+public class Server {
+	
+	/**
+	 * Server
+	 * @param port
+	 * 
+	 * Constructor for the Server class.
+	 */
+	public Server(String port) {
+		
+	}
+	
+	/**
+	 * start
+	 * 
+	 * Start listening with the server.
+	 */
+	public void start() {
+		
+	}
+	
+	/**
+	 * close
+	 * 
+	 * Stop listening with the server.
+	 */
+	public void close() {
+		
+	}
+}
