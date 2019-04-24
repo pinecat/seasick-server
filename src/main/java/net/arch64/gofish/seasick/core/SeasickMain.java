6d8dda453a686c75c82e43035dc7a7f28698ac19
JoeDivinagracia <joediv789@gmail.com>
2019-04-24 12:23:03 -0400

    Added new tests for the ProfilePage

--- src/main/java/net/arch64/gofish/seasick/core/SeasickMain.java
+++ src/main/java/net/arch64/gofish/seasick/core/SeasickMain.java
@@ -10,6 +10,7 @@ package net.arch64.gofish.seasick.core;
 /* Imports */
 import java.sql.*;
 import net.arch64.gofish.seasick.users.*;
+import net.arch64.gofish.seasick.queries.*;
 import net.arch64.gofish.seasick.serve.*;
 
 /* Class: Main */
@@ -25,16 +26,22 @@ public class SeasickMain {
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws SQLException {
+		Config conf = new Config("seasick.conf");
+		Query query = new Query(conf);
+		
 		/* database testing */
 		/* User insertion and deletion testing */
 		/*
-		Config conf = new Config("seasick.conf");
-		Query query = new Query(conf);
 		User user = new User("testty", "idklol", "gimme@gimme.net", "Hallo", "vad", false);
 		query.insertUser(user);
 		query.dropUser(user);
 		query.close();
 		*/
+		
+		/*Testing for updating and reading data for the profile page*/
+		ProfilePageQuery profile = new ProfilePageQuery(conf);
+		profile.getProfileData(2);
+		profile.getUserID("retha@email.com");
 		
 		/* server testing */
 		System.out.println("Server started listening on port 12345....");
