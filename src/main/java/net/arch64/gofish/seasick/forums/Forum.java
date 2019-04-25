13ebdb7a9d4bf80e1777776808872f0d4e801dfa
JoeDivinagracia <joediv789@gmail.com>
2019-04-25 09:49:53 -0400

    Added a username field to the class to allow displaying of a username

--- src/main/java/net/arch64/gofish/seasick/forums/Forum.java
+++ src/main/java/net/arch64/gofish/seasick/forums/Forum.java
@@ -8,6 +8,7 @@ public class Forum {
 	private int		id;
 	private int		userId;
 	private String 	content;
+	private String 	username;
 	private double	rating;
 	
 	/**
@@ -23,10 +24,12 @@ public class Forum {
 	public int getId() { return id; }
 	public int getUserId() { return userId; }
 	public String getContent() { return content; }
+	public String getUsername() { return username; }
 	public double getRating() { return rating; }
 	
 	public void setId(int id) { this.id = id; }
 	public void setUserId(int userId) { this.userId = userId; }
 	public void setContent(String content) { this.content = content; }
+	public void setUsername(String username) { this.username = username; }
 	public void setRating(double rating) { this.rating = rating; }
 }
